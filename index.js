f5054dde08d043565613ad01fe3e4f8425b7fd3d
QinDachang <qindachang@QinDachang.local>
2020-06-10 19:31:37 +0800

    fix: MINIPTOGRAM_TYPE is undefined problem

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ import { NativeModules } from 'react-native';
 
 const { ReactNativeMiniprogram } = NativeModules;
 
+export const MINIPTOGRAM_TYPE = {
+    MINIPTOGRAM_TYPE_RELEASE: 0,
+    MINIPROGRAM_TYPE_TEST: 1,
+    MINIPROGRAM_TYPE_PREVIEW: 2
+  }
+
 export default ReactNativeMiniprogram;
